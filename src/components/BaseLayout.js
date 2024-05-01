e1f41576d005646d5041cc199246c815a9d78146
Mutahira-kousar <131968891+Mutahira-kousar@users.noreply.github.com>
2024-05-01 20:56:44 +0500

    Update BaseLayout.js

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -45,7 +45,7 @@ export default function BaseLayout() {
                <Box component={'footer'} display={'flex'} flexDirection={'column'} alignItems={'center'}
                     py={'1.5rem'} sx={{opacity: 0.7}} width={'100%'}>
                   <p> 
-                     M Sohail Portfolio
+                    Mutahira Portfolio
                   </p>
                  
                </Box>
